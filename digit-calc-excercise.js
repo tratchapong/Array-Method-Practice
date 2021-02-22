c4a68d749e02ff12cfadbda56b63f4ebdd019f87
tratchapong <tratchapong@gmail.com>
2021-02-22 22:39:43 +0700

    add exercise digit-sum

---
+++ digit-calc-excercise.js
@@ -0,0 +1,26 @@
+
+// แบบฝึกหัด: หาผลรวมของเลขโดดที่น้อยที่สุด ที่มีค่า 2 เท่าของผลรวมเลขโดด input / 22Feb21 
+// เลขผลลัพธ์นั้นต้องมีค่ามากกว่า n
+
+function calc(n) {
+  let s = [...String(n)]
+  let r = s.reduce( (a,c) => a+Number(c),0) * 2
+  let rs = String(r%9==0? '' : r%9) + String('9').repeat(Math.floor(r/9))
+  let x = Number(rs)
+  if(x<n) x=n
+    while( [...String(x)].reduce( (a,c) => a+Number(c),0) <r )   { 
+      x +=1      
+    }
+  return x
+}
+
+console.log(calc(99))
+console.log(calc(10))
+console.log(calc(20))
+console.log(calc(202))
+console.log(calc(29))
+console.log(calc(90))
+
+
+
+
